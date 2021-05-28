74ff5a158255bb2e84a15b548c962c150da8f770
Ahmet Eren Sancar <aes.sncr@gmail.com>
2021-05-28 21:36:35 +0300

    homework done

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+const ejs = require('ejs');
 
-app.get('/', (req, res) => {
+app.use(express.static('public'))
 
-  const photo = {
-    id: 1,
-    name: "Blog Title",
-    description: "Blog description"
-  }
-  res.send(photo)
-})
+//Template Engine
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render('index');
+});
+
+app.get("/about", (req, res) => {
+  res.render('about');
+});
+
+app.get("/post", (req, res) => {
+  res.render('post');
+});
+
+app.get("/add_post", (req, res) => {
+  res.render('add_post');
+});
 
 const port = 3000;
 app.listen(port, () => {
